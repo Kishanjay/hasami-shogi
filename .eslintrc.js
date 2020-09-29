c458340c520cb574cfc4b43936c07639f989a2f8
Kishan Nirghin <kishan@itslogical.nl>
2020-09-29 22:41:37 +0200

    Adjust linting rules
    Embrace ESNext, Disable conflicts

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
     'prettier/vue',
     'plugin:prettier/recommended',
     'plugin:gridsome/recommended',
+    'plugin:vue/recommended',
   ],
   parserOptions: {
     ecmaVersion: 2019,
@@ -27,4 +28,34 @@ module.exports = {
       },
     },
   },
-}
+  rules: {
+    // Conflicts with prettiers max-line-length
+    'vue/max-attributes-per-line': 0,
+    'vue/singleline-html-element-content-newline': 0,
+
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'ForInStatement',
+        message:
+          'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+      },
+      // Embrace ESNext syntax
+      // {
+      //   selector: 'ForOfStatement',
+      //   message:
+      //     'iterators/generators require regenerator-runtime, which is too heavyweight for this guide to allow them. Separately, loops should be avoided in favor of array iterations.',
+      // },
+      {
+        selector: 'LabeledStatement',
+        message:
+          'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+      },
+      {
+        selector: 'WithStatement',
+        message:
+          '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+      },
+    ],
+  },
+};
